a7b3f90ad163f85aaddefe24e226b38d4570feae
Marko <markobogush@gmail.com>
2021-02-19 16:36:17 +0200

    fi1

---
+++ js/script.js
@@ -0,0 +1,102 @@
+"use strict"; 
+
+//we work in modern regime
+
+
+//DATA TYPES
+
+
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// let umaNuma = 5.6;
+
+// console.log(4 / 0); //infinity
+// console.log('string' * 0); //nan
+
+// const persone = "Alex";
+
+// const bool = false;
+
+// let aaa;
+// console.log(aaa); //undefined
+
+// const obj = {
+//     name: "Johny",
+//     age: 91,
+//     isFunny: false
+// };
+
+
+// console.log(obj.name);
+// console.log(obj["name"]); //also works but must use quotation marks but better to use the dot one
+
+
+// let img = ["plum.png", "orange.jpg", 6, "apple.bmp", {}, []];
+// console.log(img[1]);
+
+
+// alert('Hello');
+
+// const result = confirm('LAAla is Lala?');
+// console.log(result);
+
+// const answer = prompt("Do you have 81?", "81");
+// console.log(typeof(answer));
+
+//all from users is string
+
+
+// const answers = [];
+
+// answers[0] = prompt('what is your name', '');
+// answers[1] = prompt('what is your surnma', '');
+// answers[2] = prompt('how old are you', '');
+
+// console.log(typeof(answers));
+
+
+//interpolation instead of "lalala" + var + "lalala" .....
+
+// const oreh = 'tuys';
+//works only with small oblique quotation marks
+
+
+// console.log(`https://someurl.com/${oreh}/5`);
+
+// const pis = "Moishe";
+
+// alert(`Hello, ${pis}`);
+
+
+// console.log(5 + "100");
+
+let inc = 10,
+    dec = 10;
+
+// ++inc;
+// --dec;
+
+console.log(++inc);
+console.log(--dec);
+
+console.log(5%2);
+
+console.log(2 * 3 === '6');//false
+console.log(2 * 3 == '6');//klubnika
+
+
+
+const isLala = true,
+      isClose = false;
+//only when all true
+console.log(isLala && isClose); //false
+
+//if only one
+console.log(isLala || isClose); //klubnika
+
+
+
+
+
+
