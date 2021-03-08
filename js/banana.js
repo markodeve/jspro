0c38b74f11eb466b12b453ebc2dc895daa131d5f
Marko <markobogush@gmail.com>
2021-03-08 15:03:48 +0200

    dropOutitems1

--- js/banana.js
+++ js/banana.js
@@ -1,17 +1,31 @@
 // console.log('banana js cool library');
 
-const container = document.querySelector('.contain'),
-      arrow = document.querySelector('.hover_arrow'),
-      dots = document.querySelector('.dotss'),
-      list = document.querySelector('.list');
+// const container = document.querySelector('.contain'),
+//       arrow = document.querySelector('.hover_arrow'),
+//       dots = document.querySelector('.dotss'),
+//       list = document.querySelector('.list');
 
-arrow.addEventListener('click', () => {
-    container.classList.toggle('contain_up');
-    arrow.classList.toggle('chevron');
+// arrow.addEventListener('click', () => {
+//     container.classList.toggle('contain_up');
+//     arrow.classList.toggle('chevron');
+// });
+
+// dots.addEventListener('click', () => {
+//     list.classList.toggle('lista');
+// });
+
+// COOL
+
+const cool = document.querySelector('.cool'),
+    ico = document.querySelectorAll('.elem_ic'),
+    arrow = document.querySelector('.elem_ic1');
+
+cool.addEventListener('click', () => {
+    cool.classList.toggle('cool_closed');
+    for(let i = 0; i < ico.length; i++) {
+        ico[i].classList.toggle('elem_hidden');
+    }
+    arrow.classList.toggle('arrow_turn');
+    
 });
-
-dots.addEventListener('click', () => {
-    list.classList.toggle('lista');
-});
-
-
+      
