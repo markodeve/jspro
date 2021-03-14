a7511e7c98e00940bef2b86f7c6ed924dddd6a0d
Marko <markobogush@gmail.com>
2021-03-14 19:19:35 +0200

    datetime1

--- js/script2.js
+++ js/script2.js
@@ -1469,3 +1469,28 @@
 
 // console.log(arr);
 
+// const now = new Date('2021-03-14');
+
+// new Date.parse('2021-03-14');
+
+// console.log(now.setHours(18));
+// console.log(now);
+
+// console.log(now.getFullYear());
+// console.log(now.getMonth());
+// console.log(now.getDate());
+// console.log(now.getUTCHours());
+
+// console.log(now.getTimezoneOffset());
+// console.log(now.getTime());
+
+// let start = new Date();
+
+// for (let i = 0; i < 100000; i++) {
+//     let some = i ** 3;
+// }
+
+// let end = new Date();
+
+// alert(`cycle worled for ${end - start} ms`);
+
